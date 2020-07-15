ae0c49ec598572cc9296f3a7bc1f5be34763e024
Marshall Ferguson <moferg01@louisville.edu>
2020-07-15 19:57:13 -0400

    added TODOs

--- chord_progression_generator.py
+++ chord_progression_generator.py
@@ -1,1 +1,8 @@
 # Chord Progression Generator - Marshall Ferguson - 7/2020
+
+# TODO - Ask user if they want a major or minor chord progression
+
+# TODO - Ask user how many chords they want in the progression
+
+# TODO - Create a function that generates a chord progression based on user input
+    # EG - if user asks for a 4 chord progression in a major key, the program outputs something like "I IV V I" or "I ii vii I"

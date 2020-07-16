097b84611638742f4405d02f62e97c077c6dcabc
Marshall Ferguson <moferg01@louisville.edu>
2020-07-16 04:50:36 -0400

    updated TODOs

--- chord_progression_generator.py
+++ chord_progression_generator.py
@@ -4,5 +4,7 @@
 
 # TODO - Ask user how many chords they want in the progression
 
+# TODO - Create a data structure that represents the chord board graph
+
 # TODO - Create a function that generates a chord progression based on user input
     # EG - if user asks for a 4 chord progression in a major key, the program outputs something like "I IV V I" or "I ii vii I"

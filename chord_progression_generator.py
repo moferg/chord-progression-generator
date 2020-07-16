f1b74607e0cc2ee502dc4fc7af3c24ed29ef38ab
Marshall Ferguson <moferg01@louisville.edu>
2020-07-16 05:25:07 -0400

    added rough drafts of data structures for major and minor chord board graphs

--- chord_progression_generator.py
+++ chord_progression_generator.py
@@ -16,5 +16,23 @@ number_of_chords = int(number_of_chords)
 
 # TODO - Create a data structure that represents the chord board graph
 
+major_chord_board = {'I': ['ii', 'iii', 'IV', 'V', 'vi', 'vii'],
+                     'ii': ['I', 'iii', 'IV', 'V', 'vi', 'vii'],
+                     'iii': ['I', 'ii', 'IV', 'V', 'vi', 'vii'],
+                     'IV': ['I', 'ii', 'iii', 'V', 'vi', 'vii'],
+                     'V': ['I', 'ii', 'iii', 'IV', 'vi', 'vii'],
+                     'vi': ['I', 'ii', 'iii', 'IV', 'V', 'vii'],
+                     'vii': ['I', 'ii', 'IV', 'V', 'vi']
+}
+
+minor_chord_board = {'i': ['ii', 'III', 'iv', 'V', 'VI', 'VII'],
+                     'ii': ['i', 'III', 'iv', 'V', 'VI', 'VII'],
+                     'III': ['i', 'ii', 'iv', 'V', 'VI', 'VII'],
+                     'iv': ['i', 'ii', 'III', 'V', 'VI', 'VII'],
+                     'V': ['i', 'ii', 'III', 'iv', 'VI', 'VII'],
+                     'VI': ['i', 'ii', 'III', 'iv', 'V', 'VII'],
+                     'VII': ['i', 'ii', 'III', 'iv', 'V', 'VI'],
+}
+
 # TODO - Create a function that generates a chord progression based on user input
     # EG - if user asks for a 4 chord progression in a major key, the program outputs something like "I IV V I" or "I ii vii I"

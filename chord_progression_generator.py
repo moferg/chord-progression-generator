c02ea0b53f1ab847952fd7cca48de81b525bea0a
Marshall Ferguson <moferg01@louisville.edu>
2020-07-16 04:57:41 -0400

    added instructions for user and code to handle input

--- chord_progression_generator.py
+++ chord_progression_generator.py
@@ -1,8 +1,18 @@
 # Chord Progression Generator - Marshall Ferguson - 7/2020
 
+print("Welcome to the Chord Progression Generator!")
+print("This program will ask if you want a major or minor progression,")
+print("and then how many chords you want in the progression.")
+print("Then, the program will give you a randomized chord progression based on your answers!")
+
 # TODO - Ask user if they want a major or minor chord progression
 
+major_or_minor = input("Would you like the chord progression to be in a major or minor key?")
+
 # TODO - Ask user how many chords they want in the progression
+
+number_of_chords = input("How many chords would you like in the progression?")
+number_of_chords = int(number_of_chords)
 
 # TODO - Create a data structure that represents the chord board graph
 
